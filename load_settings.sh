e6fa96b7ad018184828248142f93d7caeb5c01e9
Jeremy Moon <jeremy.moon16@gmail.com>
2024-12-22 14:46:32 -0500

    fix load settings file
    Signed-off-by: Jeremy Moon <jeremy.moon16@gmail.com>

--- load_settings.sh
+++ load_settings.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-BASE_DIRECTORY="~/bash-things/"
+BASE_DIRECTORY="~/bash-things"
 
 source "${BASE_DIRECTORY}/custom_shell_prompt.sh"
 source "${BASE_DIRECTORY}/power.sh"
 alias goto=". ${BASE_DIRECTORY}/shortcuts.sh"
 
-export PROMPT_COMMAND="source ${BASE_DIRECTORY}/bash-things/custom_shell_prompt.sh"
+export PROMPT_COMMAND="source ${BASE_DIRECTORY}/custom_shell_prompt.sh"
